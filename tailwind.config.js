25bd2d61e11b8796d96c5b72dd88f6f73fa3ba7a
Jovica Šuša <jovicasusa1985@gmail.com>
2023-11-25 20:54:28 +0100

    Landing page (#1)
    * WIP - Let's go neo brutal
    
    * Improvements
    
    * Modifications
    
    * Minor fixes

--- tailwind.config.js
+++ tailwind.config.js
@@ -4,5 +4,18 @@ module.exports = {
     './app/helpers/**/*.rb',
     './app/assets/stylesheets/**/*.css',
     './app/javascript/**/*.js'
-  ]
+  ],
+  theme: {
+    extend: {
+      animation: {
+        marquee: "marquee 30s linear infinite"
+      },
+      keyframes: {
+        marquee: {
+          "0%": { transform: "translateX(0%)" },
+          "100%": { transform: "translateX(-100%)" },
+        }
+      },
+    },
+  },
 }
