133206a681f480d89f65867a3064358bdd874fe7
JovicaSusa <jovicasusa1985@gmail.com>
2023-11-26 21:08:15 +0100

    Created pair requests; views and logic

---
+++ app/controllers/users/pair_requests_controller.rb
@@ -0,0 +1,26 @@
+class Users::PairRequestsController < ApplicationController
+  def index
+    @pair_requests = current_user.pair_requests.order(created_at: :desc)
+  end
+
+  def new
+    @pair_request = current_user.pair_requests.build
+  end
+
+  def create
+    @pair_request = current_user.pair_requests.build(pair_request_params)
+
+    if @pair_request.save
+      respond_to do |format|
+        format.html { redirect_to users_pair_requests_path, notice: "Request posted! Good luck" }
+        format.turbo_stream
+      end
+    else
+      render :new, status: :unprocessable_entity
+    end
+  end
+
+  private
+
+  def pair_request_params = params.require(:pair_request).permit(:subject, :description)
+end
--- app/models/user.rb
+++ app/models/user.rb
@@ -4,4 +4,6 @@ class User < ApplicationRecord
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable,
          :confirmable
+
+  has_many :pair_requests, dependent: :destroy # TODO: Reconsider dependent option
 end
