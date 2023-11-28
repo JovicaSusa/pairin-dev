51a7a4b52aed4e0a08667b519a7d2f7d71faced8
JovicaSusa <jovicasusa1985@gmail.com>
2023-11-28 21:12:04 +0100

    Null constraint

--- db/migrate/20231127152218_create_offers.rb
+++ db/migrate/20231127152218_create_offers.rb
@@ -3,7 +3,7 @@ class CreateOffers < ActiveRecord::Migration[7.1]
     create_table :offers do |t|
       t.references :offerer, null: false, foreign_key: { to_table: :users }
       t.references :pair_request, null: false, foreign_key: true
-      t.text :message
+      t.text :message, null: false
 
       t.timestamps
     end
