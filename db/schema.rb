f9cdf55f59b98350dda10d84b780621f7ef7f1a0
JovicaSusa <jovicasusa1985@gmail.com>
2023-11-29 23:10:09 +0100

    Added accepted_at

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.1].define(version: 2023_11_29_215001) do
+ActiveRecord::Schema[7.1].define(version: 2023_11_29_220839) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -21,6 +21,7 @@ ActiveRecord::Schema[7.1].define(version: 2023_11_29_215001) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.bigint "period_id", null: false
+    t.datetime "accepted_at"
     t.index ["offerer_id", "pair_request_id"], name: "index_offers_on_offerer_id_and_pair_request_id", unique: true
     t.index ["offerer_id"], name: "index_offers_on_offerer_id"
     t.index ["pair_request_id"], name: "index_offers_on_pair_request_id"
