96cc1a534b30d7191a8dc8a6234c592230ee255a
JovicaSusa <jovicasusa1985@gmail.com>
2023-11-29 22:39:49 +0100

    Set end_at

--- app/models/period.rb
+++ app/models/period.rb
@@ -1,5 +1,13 @@
 class Period < ApplicationRecord
   belongs_to :periodable, polymorphic: true
 
-  validates :start_at, presence: true
+  validates :start_at, :end_at, presence: true
+
+  before_validation :set_end_at # TODO: Move this somewhere else(to pair request cntrl)
+
+  private
+
+  def set_end_at
+    self.end_at = self.start_at.advance(minutes: self.periodable.duration)
+  end
 end
