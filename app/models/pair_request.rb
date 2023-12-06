a84b4a1b3d2337c1dddf94a2d2534f4a4940f4d3
JovicaSusa <jovicasusa1985@gmail.com>
2023-12-06 18:55:49 +0100

    Add tags to pair request

--- app/models/pair_request.rb
+++ app/models/pair_request.rb
@@ -3,10 +3,13 @@ class PairRequest < ApplicationRecord
   has_many :offers, dependent: nil # TODO: Reconsider
   has_many :periods, as: :periodable, dependent: :destroy
   has_many :sessions, as: :sessionable
+  has_many :taggings, as: :taggable
+  has_many :tags, through: :taggings
 
 
   validates :description, :subject, :duration, presence: true
   validates :duration, numericality: { greater_than: 0 }
 
   accepts_nested_attributes_for :periods, reject_if: :all_blank, allow_destroy: true
+  accepts_nested_attributes_for :taggings, reject_if: :all_blank, allow_destroy: true
 end
