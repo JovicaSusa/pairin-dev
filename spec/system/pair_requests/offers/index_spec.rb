b45b34a07e800133fe03c0754e836fa96d55b79b
JovicaSusa <jovicasusa1985@gmail.com>
2023-12-06 15:23:28 +0100

    Added offers index spec

---
+++ spec/system/pair_requests/offers/index_spec.rb
@@ -0,0 +1,16 @@
+RSpec.describe "view pair request offers", type: :system do
+  let(:current_user) { create(:user) }
+
+  before { sign_in(current_user) }
+
+  let(:pair_request) { create(:pair_request, current_user: current_user) }
+  let!(:offer_for_pair_request) { create(:offer, offerable: pair_request, message: "Hey!") }
+  let!(:other_offer) { create(:offer, message: "Ho!") }
+
+  it "displays offers for given pair request" do
+    visit pair_request_offers(pair_request)
+
+    expect(page).to have_content("Hey")
+    expect(page).not_to have_content("Ho")
+  end
+end
