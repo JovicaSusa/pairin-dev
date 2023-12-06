545def48ed4e94e44baca21d52b185ec4635200e
JovicaSusa <jovicasusa1985@gmail.com>
2023-12-06 15:48:59 +0100

    Fix spec

--- spec/system/pair_requests/offers/index_spec.rb
+++ spec/system/pair_requests/offers/index_spec.rb
@@ -3,12 +3,12 @@ RSpec.describe "view pair request offers", type: :system do
 
   before { sign_in(current_user) }
 
-  let(:pair_request) { create(:pair_request, current_user: current_user) }
-  let!(:offer_for_pair_request) { create(:offer, offerable: pair_request, message: "Hey!") }
+  let(:pair_request) { create(:pair_request, user: current_user) }
+  let!(:offer_for_pair_request) { create(:offer, pair_request:, message: "Hey!") }
   let!(:other_offer) { create(:offer, message: "Ho!") }
 
   it "displays offers for given pair request" do
-    visit pair_request_offers(pair_request)
+    visit pair_request_offers_path(pair_request)
 
     expect(page).to have_content("Hey")
     expect(page).not_to have_content("Ho")
