3e1ebfa08f62cdb8cbba5db40ab46aaefac86eb9
JovicaSusa <jovicasusa1985@gmail.com>
2024-11-09 11:07:23 +0100

    Added indexes

--- db/migrate/20241108112714_add_users_details.rb
+++ db/migrate/20241108112714_add_users_details.rb
@@ -3,10 +3,10 @@ class AddUsersDetails < ActiveRecord::Migration[7.1]
     change_table :users do |t|
       t.string :language, index: true
       t.text :about
-      t.string :country
-      t.datetime :date_of_birth
+      t.string :country, index: true
+      t.datetime :date_of_birth, index: true
       t.string :level
-      t.datetime :programming_since
+      t.datetime :programming_since, index: true
       t.text :image_data
     end
   end
--- db/schema.rb
+++ db/schema.rb
@@ -102,8 +102,11 @@ ActiveRecord::Schema[7.1].define(version: 2024_11_08_112714) do
     t.datetime "programming_since"
     t.text "image_data"
     t.index ["confirmation_token"], name: "index_users_on_confirmation_token", unique: true
+    t.index ["country"], name: "index_users_on_country"
+    t.index ["date_of_birth"], name: "index_users_on_date_of_birth"
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["language"], name: "index_users_on_language"
+    t.index ["programming_since"], name: "index_users_on_programming_since"
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
 
