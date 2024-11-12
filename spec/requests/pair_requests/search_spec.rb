4791e49bf31f7b30c4edc975153dd93b60e2efa8
JovicaSusa <jovicasusa1985@gmail.com>
2024-11-12 23:50:15 +0100

    Give up on request spec

--- spec/requests/pair_requests/search_spec.rb
+++ spec/requests/pair_requests/search_spec.rb
@@ -2,20 +2,6 @@ require 'rails_helper'
 
 RSpec.describe "PairRequests", type: :request do
   describe "GET /search" do
-    it "performs expected search" do
-      get "/pair_requests/search",
-        params: {
-          q: {
-            tags_name_eq: "Ruby",
-            q_duration_eq: 45,
-            q_user_level_eq: "beginner",
-            q_user_language_eq: "SR",
-            q_periods_start_at_gteq: 5.days.from_now,
-            q_periods_end_at_lteq: 6.days.from_now,
-          }
-        }
-
-      # TODO: Add exceptions
-    end
+    # TODO: Check what to test
   end
 end
