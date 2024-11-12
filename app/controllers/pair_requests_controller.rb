56c4add3a8d1aecce90fb9cfd4402629ed518b59
JovicaSusa <jovicasusa1985@gmail.com>
2024-11-12 23:41:14 +0100

    Fixes; system specs with all params

--- app/controllers/pair_requests_controller.rb
+++ app/controllers/pair_requests_controller.rb
@@ -1,14 +1,13 @@
 class PairRequestsController < ApplicationController
   def index
     @q = PairRequest.ransack(params[:q])
-
     @pagy, @pair_requests = pagy_countless(PairRequest.where.not(user_id: current_user.id).all)
 
     render "scrollable_list" if params[:page]
   end
 
   def search
-    @q = PairRequest.left_joins(:tags).ransack(params[:q].compact_blank)
+    @q = PairRequest.left_joins(:tags, :periods, :user).ransack(params[:q].compact_blank)
     @pagy, @pair_requests = pagy_countless(@q.result(distinct: true))
 
     render "scrollable_list" if params[:page]
--- spec/system/pair_requests/search_spec.rb
+++ spec/system/pair_requests/search_spec.rb
@@ -3,44 +3,31 @@ RSpec.describe "search pair requests", type: :system, js: true do
 
   before { sign_in(current_user) }
 
-  context "search by tag name" do
-    let!(:pair_request_with_searched_tag) { create(:pair_request, subject: "AAA") }
-    let!(:tagging) { create(:tagging, taggable: pair_request_with_searched_tag, tag: tag) }
-    let!(:tag) { create(:tag, name: "Ruby") }
-    let!(:not_searched_pair_request) { create(:pair_request, subject: "BBB") }
+  context "search by all params" do
+    let(:searched_pair_request) { create(:pair_request, subject: "Test", user: user, duration: 45) }
+    let(:user) { create(:user, language: "EN", level: "novice") }
+
+    before do
+      tag = create(:tag, name: "Ruby")
+      create(:tagging, taggable: searched_pair_request, tag: tag)
+      create(:period, periodable: searched_pair_request, start_at: 2.minutes.from_now)
+    end
 
     it "displays pair requests tagged by searched tag" do
       visit pair_requests_path
 
       fill_in "q_tags_name_eq", with: "Ruby"
-      click_button "Go"
-
-      expect(page).to have_content("AAA")
-      expect(page).not_to have_content("BBB")
-    end
-  end
-
-  context "pagination" do
-    let!(:pair_request_one) { create(:pair_request, subject: "AAA", duration: 45) }
-    let!(:pair_request_two) { create(:pair_request, subject: "BBB", duration: 45) }
-    let!(:pair_request_three) { create(:pair_request, subject: "CCC", duration: 60) }
-
-    before do
-      Pagy::DEFAULT[:limit] = 1
-    end
-
-    it "properly paginates result" do
-      visit pair_requests_path
-
       fill_in "q_duration_eq", with: 45
+      find("#q_user_level_eq").click
+      find("li[data-value='novice']").click
+      find("#q_user_language_eq").click
+      find("li[data-value='EN']").click
+      # fill_in "q_user_language_eq", with: "EN"
+      fill_in "q_periods_start_at_gteq", with: 1.minute.from_now.to_s
+      fill_in "q_periods_end_at_lteq", with: 3.minutes.from_now.to_s
       click_button "Go"
 
-      expect(page).to have_content("AAA")
-      expect(page).not_to have_content("BBB")
-
-      scroll_to("#pair-requests-page-2")
-
-      expect(page).to have_content("BBB")
+      expect(page).to have_content("Test")
     end
   end
 end
