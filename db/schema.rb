f25482a4c87cf0295455714ab554fb998cc30fe4
JovicaSusa <jovicasusa1985@gmail.com>
2024-11-15 09:42:34 +0100

    Added user name and title

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.1].define(version: 2024_11_08_112714) do
+ActiveRecord::Schema[7.1].define(version: 2024_11_15_082033) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -111,6 +111,8 @@ ActiveRecord::Schema[7.1].define(version: 2024_11_08_112714) do
     t.string "level"
     t.datetime "programming_since"
     t.text "image_data"
+    t.string "name"
+    t.string "title"
     t.index ["confirmation_token"], name: "index_users_on_confirmation_token", unique: true
     t.index ["country"], name: "index_users_on_country"
     t.index ["date_of_birth"], name: "index_users_on_date_of_birth"
