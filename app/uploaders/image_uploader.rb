ac7f1f6457c904399e9f67809fcd772eb58f1cf4
JovicaSusa <jovicasusa1985@gmail.com>
2024-11-15 09:16:30 +0100

    UI Improvements - wip

--- app/uploaders/image_uploader.rb
+++ app/uploaders/image_uploader.rb
@@ -1,6 +1,12 @@
 require "image_processing/mini_magick"
 
 class ImageUploader < Shrine
+  plugin :default_url
+
+  Attacher.default_url do |**options|
+    "/uploads/store/#{name}/default.png"
+  end
+
   Attacher.derivatives do |original|
     magick = ImageProcessing::MiniMagick.source(original)
 
