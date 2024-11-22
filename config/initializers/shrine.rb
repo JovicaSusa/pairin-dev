1bb42d1dca9b6134b6d67bbde72fd763c652cb4e
Jovica Šuša <jovicasusa1985@gmail.com>
2024-11-22 12:41:38 +0100

    Aws s3/vips (#38)
    * Setup shrine
    
    * Add missing require
    
    * Fix creds
    
    * Use tmp folder for cache
    
    * Fix creds
    
    * Fix deploy issue
    
    * Added imagemagick
    
    * Install imagemagick
    
    * Try to fix imagemagick
    
    * Use vips

--- config/initializers/shrine.rb
+++ config/initializers/shrine.rb
@@ -1,9 +1,22 @@
 require "shrine"
 require "shrine/storage/file_system"
+require "shrine/storage/s3"
+
+permanent_storage =
+  if Rails.env.production?
+    Shrine::Storage::S3.new(
+      bucket: "pairin",
+      region: "eu-north-1",
+      access_key_id: Rails.application.credentials.aws&.access_key_id,
+      secret_access_key: Rails.application.credentials.aws&.secret_access_key
+    )
+  else
+    Shrine::Storage::FileSystem.new("public", prefix: "uploads")
+  end
 
 Shrine.storages = {
-  cache: Shrine::Storage::FileSystem.new("public", prefix: "uploads/cache"), # temporary
-  store: Shrine::Storage::FileSystem.new("public", prefix: "uploads"),       # permanent
+  cache: Shrine::Storage::FileSystem.new("tmp", prefix: "uploads/cache"),
+  store: permanent_storage
 }
 
 Shrine.plugin :activerecord           # loads Active Record integration
