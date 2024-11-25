d200200768ec34c6c1b8739898ab28518a8b019e
JovicaSusa <jovicasusa1985@gmail.com>
2024-11-25 13:35:35 +0100

    Send emails with mailjet

--- app/models/user.rb
+++ app/models/user.rb
@@ -5,8 +5,8 @@ class User < ApplicationRecord
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
-         :recoverable, :rememberable, :validatable
-        #  :confirmable NOTE: Disable for now
+         :recoverable, :rememberable, :validatable,
+         :confirmable
 
   has_many :pair_requests, dependent: :destroy # TODO: Reconsider dependent option
   has_many :offers, foreign_key: :offerer_id, dependent: nil # TODO: Reconsider dependent option
