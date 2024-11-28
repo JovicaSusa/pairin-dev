feaf99e7feeae2228c07d156f5f59cc348f49b71
JovicaSusa <jovicasusa1985@gmail.com>
2024-11-28 23:46:46 +0100

    Change name for envs

--- app/controllers/mission_control_auth_controller.rb
+++ app/controllers/mission_control_auth_controller.rb
@@ -1,3 +1,3 @@
 class MissionControlAuthController < ApplicationController
-  http_basic_authenticate_with name: ENV["BASIC_AUTH_JOBS_NAME"], password: ENV["BASIC_AUTH_JOBS_PASSWORD"]
+  http_basic_authenticate_with name: ENV["MISSION_CONTROL_NAME"], password: ENV["MISSION_CONTROL_PASSWORD"]
 end
