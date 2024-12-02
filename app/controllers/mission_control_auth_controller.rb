b6ed657665df978f1bf8efa4b7e53de70bc447ab
JovicaSusa <jovicasusa1985@gmail.com>
2024-12-02 08:06:20 +0100

    Comment out missing control auth for now

--- app/controllers/mission_control_auth_controller.rb
+++ app/controllers/mission_control_auth_controller.rb
@@ -1,3 +1,3 @@
 class MissionControlAuthController < ApplicationController
-  http_basic_authenticate_with name: ENV["MISSION_CONTROL_NAME"], password: ENV["MISSION_CONTROL_PASSWORD"]
+  # http_basic_authenticate_with name: ENV["MISSION_CONTROL_NAME"], password: ENV["MISSION_CONTROL_PASSWORD"]
 end
