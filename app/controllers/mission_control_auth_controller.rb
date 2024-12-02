56a9af5f754c3327c3b96b4978753f70080bafd2
JovicaSusa <jovicasusa1985@gmail.com>
2024-12-02 23:30:40 +0100

    Not sure why it's missing

--- app/controllers/mission_control_auth_controller.rb
+++ app/controllers/mission_control_auth_controller.rb
@@ -1,6 +1,6 @@
 class MissionControlAuthController < ApplicationController
   http_basic_authenticate_with(
-    name: Rails.application.credentials.mission_control.name,
-    password: Rails.application.credentials.mission_control.password
+    name: Rails.application.credentials.mission_control&.name,
+    password: Rails.application.credentials.mission_control&.password
   )
 end
