1bae63b9408558687f74128590b0e3f11221f338
JovicaSusa <jovicasusa1985@gmail.com>
2024-12-04 00:35:29 +0100

    Validate image upload

--- app/uploaders/image_uploader.rb
+++ app/uploaders/image_uploader.rb
@@ -3,6 +3,12 @@ require "image_processing/vips"
 class ImageUploader < Shrine
   plugin :default_url
 
+  Attacher.validate do
+    validate_max_size 1*1024*1024
+    validate_mime_type %w[image/jpeg image/png image/webp image/tiff]
+    validate_extension %w[jpg jpeg png webp tiff tif]
+  end
+
   Attacher.default_url do |**options|
     ActionController::Base.helpers.asset_path('logo.svg')
   end
