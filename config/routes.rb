3d39b6e093a2c5935f2f5d925adabff340d7f11b
Jovica Šuša <jovicasusa1985@gmail.com>
2024-12-03 23:44:28 +0100

    Rack attack + honeypot (#48)

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 Rails.application.routes.draw do
   mount MissionControl::Jobs::Engine, at: "/jobs"
-  devise_for :users
+  devise_for :users, controllers: { registrations: "users/registrations" }
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Reveal health status on /up that returns 200 if the app boots with no exceptions, otherwise 500.
