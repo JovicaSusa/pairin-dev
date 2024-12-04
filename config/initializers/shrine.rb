219a67fbfd3f902dbf011b537a8000f33e63554c
Jovica Šuša <jovicasusa1985@gmail.com>
2024-12-04 09:16:27 +0100

    Validate image upload (#50)

--- config/initializers/shrine.rb
+++ config/initializers/shrine.rb
@@ -23,3 +23,4 @@ Shrine.plugin :activerecord           # loads Active Record integration
 Shrine.plugin :cached_attachment_data # enables retaining cached file across form redisplays
 Shrine.plugin :restore_cached_data    # extracts metadata for assigned cached files
 Shrine.plugin :derivatives, create_on_promote: true
+Shrine.plugin :validation_helpers
