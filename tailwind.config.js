a55556d8a22b4293f2d27870e5998f62f0177f90
JovicaSusa <jovicasusa1985@gmail.com>
2025-04-02 10:03:55 +0200

    WIP - Add ripple ui

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,4 +1,5 @@
 module.exports = {
+  /** @type {import('rippleui').Config} */
   content: [
     './app/views/**/*.html.erb',
     './app/helpers/**/*.rb',
@@ -6,32 +7,48 @@ module.exports = {
     './app/javascript/**/*.js',
     'config/initializers/simple_form.rb'
   ],
-  theme: {
-    extend: {
-      colors: {
-        orange: "#FF8B37",
-        purple: "#7B5FF1",
-        green: "#55D087",
-        background: "#E3E3FF"
-      },
-      fontFamily: {
-        "sans": ["Cabin", "sans-serif"],
-        "headline": ["Work Sans"]
-      },
-      animation: {
-        marquee: "marquee 120s linear infinite",
-        appearThenFade: "appearThenFade 5s linear"
-      },
-      keyframes: {
-        marquee: {
-          "0%": { transform: "translateX(0%)" },
-          "100%": { transform: "translateX(-100%)" },
-        },
-        appearThenFade: {
-          "0%, 100%": { opacity: 0 },
-          "5%, 60%": { opacity: 1 }
-        }
-      },
-    },
-  },
+  plugins: [require("rippleui")],
+  rippleui: {
+    defaultStyle: false,
+    removeThemes: ["dark"],
+    themes: [
+      {
+        themeName: "light",
+        colorScheme: "light",
+        prefersColorScheme: true,
+				colors: {
+					primary: "#235264",
+					backgroundPrimary: "#964643",
+				},
+      }
+    ]
+  }
+  // theme: {
+  //   extend: {
+  //     colors: {
+  //       orange: "#FF8B37",
+  //       purple: "#7B5FF1",
+  //       green: "#55D087",
+  //       background: "#E3E3FF"
+  //     },
+  //     fontFamily: {
+  //       "sans": ["Cabin", "sans-serif"],
+  //       "headline": ["Work Sans"]
+  //     },
+  //     animation: {
+  //       marquee: "marquee 120s linear infinite",
+  //       appearThenFade: "appearThenFade 5s linear"
+  //     },
+  //     keyframes: {
+  //       marquee: {
+  //         "0%": { transform: "translateX(0%)" },
+  //         "100%": { transform: "translateX(-100%)" },
+  //       },
+  //       appearThenFade: {
+  //         "0%, 100%": { opacity: 0 },
+  //         "5%, 60%": { opacity: 1 }
+  //       }
+  //     },
+  //   },
+  // },
 }
