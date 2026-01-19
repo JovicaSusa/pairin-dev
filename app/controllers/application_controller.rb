d499c56cc04d909310ec7ec6a6dd1eb207a5d799
markok96 <34298276+markok96@users.noreply.github.com>
2026-01-19 21:27:19 +0100

    created applayout and flash components

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,9 +2,7 @@ class ApplicationController < ActionController::Base
   include Pagy::Backend
   include Pundit::Authorization
 
-  inertia_share auth: -> {
-    { user: current_user } 
-  }
+  inertia_share auth: -> { { user: current_user } }, flash: -> { flash.to_hash }
 
   def after_sign_in_path_for(resource_or_scope)
     pair_requests_path
---
+++ app/javascript/layouts/AppLayout.jsx
@@ -0,0 +1,16 @@
+import FlashMessages from '@/components/FlashMessages';
+import MainNav from "@/components/MainNav";
+import { usePage } from '@inertiajs/react';
+
+export default function AppLayout({ children }) {
+  const { url, props } = usePage()
+  const currentUser = props.auth?.user;
+
+  return (
+    <main className='relative bg-yellow-50 min-h-screen'>
+      <FlashMessages />
+      <MainNav currentUser={currentUser} currentPath={url} />
+      <article className="md:ml-[16.666667%] min-h-screen">{children}</article>
+    </main>
+  );
+}
